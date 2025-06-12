9bce2d5d69acc14d2679e36cb13f00015a20b264
Omar Farouk <Komarf28@gmail.com>
2025-06-12 07:58:11 +0000

    a

--- obj/Debug/net9.0/BlazorSuperApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorSuperApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorSuperApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6a6688d96d13c590e8401940fd8ec1f04f452b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorSuperApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorSuperApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
